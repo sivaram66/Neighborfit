7a290f865cf65a7732fa73809bd1dc753acf2da3
Siva Ram <sivaramlikeu@gmail.com>
2025-07-09 23:11:45 +0530

    Updated Setting.py

--- Neighborfit/settings.py
+++ Neighborfit/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-ta$u56cyrjladf2r_v@^l$gwvkp*$c@2d-65b+#aoxrr@m**nn'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1 localhost").split()
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 DEBUG = True
 
